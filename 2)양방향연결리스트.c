1cccf0cd569029e300c0fb3fdab4e05775de6d24
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-01-10 15:51:22 +0900

    Update 2)양방향연결리스트.c
    1)앞 뒤에 노드에 데이터를 저장할 수 있다
    2)앞, 뒤로 데이터를 접근할 수 있습니다.
     단점. 많은 데이터를 할당해야 하는 단점이 있다.

--- 2)양방향연결리스트.c
+++ 2)양방향연결리스트.c
@@ -2,33 +2,78 @@
 #include <stdlib.h>
 
 typedef struct {
-
-	int data;
+	
+	int *data;
+	struct Node *next;
 	struct Node* prev;
-	struct Node* next;
-
 
 
 }Node;
 
-Node* head, *tail;
+Node* head,*tail;
 
-int insert(int data) {
+
+void insert(int data) {//오름차순 출력 및 양방향연결리스트
 	Node* node = (Node*)malloc(sizeof(Node));
 	node->data = data;
 	Node* cur;
 	cur = head->next;
 	while (cur->data < data && cur != NULL) {
-
 		cur = cur->next;
-
 	}
-
 	Node* prev = cur->prev;
 	prev->next = node;
 	node->prev = prev;
 	cur->prev = node;
 	node->next = cur;
+
+
+
+}
+void removeFront() { // 첫번째 노드 삭제
+	Node* node = head->next;
+	head->next = node->next;
+	Node* next = node->next;
+	node->prev = head;
+	free(node);
+}
+
+void show() { //출력함수
+	Node* cur = head->next;
+	while (cur != tail) {
+		printf("%d ", cur->data);
+		cur = cur->next;
+	}
+	printf("\n");
+
+
+
+}
+
+
+int main(void) {
+	head = (Node*)malloc(sizeof(Node));
+	tail = (Node*)malloc(sizeof(Node));
+	head->next = tail;
+	head->prev = head;
+	tail->next = tail;
+	tail->prev = head;
+	insert(5);
+	insert(4);
+	insert(3);
+	insert(9);
+	insert(15);
+	delet();
+	show();
+
+	system("pause");
+	return 0;
+	
+
+
+
+
+
 
 
 
